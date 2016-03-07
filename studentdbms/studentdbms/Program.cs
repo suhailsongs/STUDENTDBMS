5d3ae8d3e76979eca5ec38ff3ba094ba1af9e066
Suhail <suhailsongs@gmail.com>
2016-03-07 22:20:09 +0530

    Modified program--check duplicate data entry...

--- studentdbms/studentdbms/Program.cs
+++ studentdbms/studentdbms/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.IO;
 
 namespace StudentDBMS
@@ -6,9 +10,10 @@ namespace StudentDBMS
 
     class student
     {
-        public static int op;
-        public static string path = @"C:/Users/Tasneem Parkar/Desktop/SDetails.txt";
-        public static void displaymenu()
+        int op;
+        string path = @"C:/Users/student/Desktop/SDetails.txt";
+
+        public int displaymenu()
         {
 
             Console.WriteLine(" WELCOME TO STUDENT DBMS ");
@@ -20,14 +25,11 @@ namespace StudentDBMS
             Console.WriteLine("5. Show all");
             Console.WriteLine("6. Exit");
             Console.WriteLine(" ********************************** ");
-        }
 
-        public static int option()
-        {
             return (op = Convert.ToInt32(Console.ReadLine()));
         }
 
-        public static void AddRecord()
+        public void AddRecord()
         {
             int Rollno;
             String Name, Address;
@@ -37,10 +39,13 @@ namespace StudentDBMS
             Console.WriteLine(" ************************************* ");
             Console.Write(" Enter Your Rollno : ");
             Rollno = Convert.ToInt32(Console.ReadLine());
+
+
             Console.Write(" Enter Your Name : ");
             Name = Convert.ToString(Console.ReadLine());
             Console.Write(" Enter Your Address : ");
             Address = Convert.ToString(Console.ReadLine());
+
 
 
             StreamWriter sw;
@@ -49,6 +54,7 @@ namespace StudentDBMS
                 if (File.Exists(path))
                 {
                     sw = new StreamWriter(new FileStream(path, FileMode.Append, FileAccess.Write));
+
                 }
                 else
                 {
@@ -76,10 +82,17 @@ namespace StudentDBMS
             }
 
             sw.Close();
-            Console.ReadLine();
+
+
         }
 
-        public static void ShowAll()
+
+
+
+
+
+
+        public void ShowAll()
         {
 
             StreamReader br;
@@ -119,12 +132,12 @@ namespace StudentDBMS
 
         }
 
-        public static void Exit()
+        public void Exit()
         {
             Environment.Exit(1);
         }
 
-        public static void Search()
+        public void Search()
         {
             StreamReader br;
 
@@ -132,7 +145,7 @@ namespace StudentDBMS
             Console.WriteLine("************************************ ");
             Console.Write("Enter Roll No. to Search Record: ");
             int val = Convert.ToInt32(Console.ReadLine());
-            string[] details = new string[100];
+            string[] details = new string[10];
 
             try
             {
@@ -181,16 +194,27 @@ namespace StudentDBMS
             br.Close();
         }
 
-        public static void Delete()
+        public void Delete()
         {
-                StreamReader br; 
-                StreamWriter bw;
+            StreamReader br;
+            StreamWriter bw;
 
-                Console.WriteLine("Welcome to Student Management System");
-                Console.WriteLine("_______________________________________");
-                Console.Write("Enter Roll No. to Delete Record: ");
-                int val = Convert.ToInt32(Console.ReadLine());
-                string[] details = new string[100];
+            Console.WriteLine("Welcome to Student Management System");
+            Console.WriteLine("_______________________________________");
+            Console.Write("Enter Roll No. to Delete Record: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            string[] details = new string[100];
+
+            try
+            {
+                bw = new StreamWriter(new FileStream("C:/Users/Tasneem Parkar/Desktop/SDetails2222.txt", FileMode.OpenOrCreate));
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + " cannot create file");
+                return;
+            }
 
             try
             {
@@ -202,101 +226,144 @@ namespace StudentDBMS
                 String[] rec = line.Split(deli);
                 for (int val1 = 0; val1 < rec.Length; val1++)
                 {
-                    char deli2 = ';';
-
-                    if (rec[val1].Contains(Convert.ToString(val)))
+                    if (!rec[val1].Contains(Convert.ToString(val)))
                     {
-                        String[] field = rec[val1].Split(deli2);
-                        for (int val2 = 0; val2 < field.Length; val2++)
-                        {
-                            details[val2] = field[val2];
-                        }
-                        Console.Write("\n");
+                        bw.Write(rec[val1]);
+                    }
+                    bw.Write("|");
+                }
+                Console.WriteLine("Record has been deleted successfully.");
+                Console.ReadKey();
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + " cannot open file");
+                return;
+            }
+
+            br.Close();
+            bw.Close();
+            File.Replace("C:/Users/student/Desktop/SDetails2222.txt", path, "backup.txt", false);
+        }
+
+
+        public void Update()
+        {
+            StreamReader br;
+            StreamWriter bw;
+
+            Console.WriteLine(" WELCOME TO STUDENT DBMS ");
+            Console.WriteLine("************************************ ");
+            Console.Write("Enter Roll No. to Update Record: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add new record ");
+            Console.WriteLine("_______________________________________");
+
+            Console.Write("Enter Name: ");
+            string name = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter Address: ");
+            string address = Convert.ToString(Console.ReadLine());
+
+            string[] details = new string[100];
+
+            try
+            {
+                bw = new StreamWriter(new FileStream("C:/Users/student/Desktop/SDetails222.txt", FileMode.OpenOrCreate));
+            }
+
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + " cannot create file");
+                return;
+            }
+
+            try
+            {
+                br = new StreamReader(new FileStream(path, FileMode.Open));
+                string line = br.ReadLine();
+                Console.Write("\n");
+
+                char deli = '|';
+                String[] rec = line.Split(deli);
+                for (int val1 = 0; val1 < rec.Length; val1++)
+                {
+                    if (!rec[val1].Contains(Convert.ToString(val)))
+                    {
+                        bw.Write(rec[val1]);
                     }
                     else
                     {
-                        try
-                        {
-                            bw = new StreamWriter(new FileStream("C:/Users/Tasneem Parkar/Desktop/SDetails22222222222.txt", FileMode.OpenOrCreate));
-                        }
-
-                        catch (IOException e)
-                        {
-                            Console.WriteLine(e.Message + " cannot create file");
-                            return;
-                        }
-                        try
-                        {
-                            bw.Write(rec[val1] + "|");
-                        }
-                        catch (IOException e)
-                        {
-                            Console.WriteLine(e.Message + " Cannot write into the file");
-                            return;
-                        }
-                        bw.Close();
+                        bw.Write(val + ";" + name + ";" + address);
                     }
-
+                    bw.Write("|");
                 }
+                Console.WriteLine("Record has been updated successfully.");
 
             }
-            catch ( IOException e)
+
+            catch (IOException e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Message + " cannot open file");
+                return;
             }
-        }   
+            Console.ReadKey();
+            br.Close();
+            bw.Close();
+            File.Replace("C:/Users/Tasneem Parkar/Desktop/SDetails222.txt", path, "backup.txt", false);
+        }
+
+
+    }
 
 
 
 
+    class Program
+    {
 
-        class Program
+        static void Main(string[] args)
         {
             student std = new student();
-            static void Main(string[] args)
+            char ch = 'y';
+            while (ch == 'y' || ch == 'Y')
             {
-                char ch = 'y';
-                while (ch == 'y' || ch == 'Y')
+
+                Console.Clear();
+                // std.displaymenu();
+
+                int o = std.displaymenu();
+
+                switch (o)
                 {
-                    Console.Clear();
-                    student.displaymenu();
+                    case 1:
+                        std.AddRecord();
+                        break;
 
-                    int o = student.option();
-
-                    switch (o)
-                    {
-                        case 1:
-                            student.AddRecord();
-                            break;
-
-                        case 2:
-                            //p.UpdateRecord();
-                            break;
-                        case 3:
-                            student.Delete();
-                            break;
-                        case 4:
-                            student.Search();
-                            break;
-                        case 5:
-                            student.ShowAll();
-                            break;
-                        case 6:
-                            student.Exit();
-                            break;
-                        default:
-                            Console.WriteLine("Invalid Selection");
-                            break;
-                    }
-                    Console.WriteLine("Do you want to add another record (Y/N)? ");
-
-                    ch = Convert.ToChar(Console.ReadLine());
-
-                    if (ch == 'Y' || ch == 'y')
-                        continue;
-                    else
+                    case 2:
+                        std.Update();
+                        break;
+                    case 3:
+                        std.Delete();
+                        break;
+                    case 4:
+                        std.Search();
+                        break;
+                    case 5:
+                        std.ShowAll();
+                        break;
+                    case 6:
+                        std.Exit();
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Selection");
                         break;
                 }
+                Console.WriteLine("Do you want to continue  (Y/N)? ");
+
+                ch = Convert.ToChar(Console.ReadLine());
+
             }
         }
     }
